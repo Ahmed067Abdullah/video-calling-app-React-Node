d04103fd78922cfc3b4a4a7b8e8350541f956f5c
Ahmed067Abdullah <ahmed@sudofy.com>
2020-08-15 00:29:13 +0500

    dynamic port added for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const users = {};
 
