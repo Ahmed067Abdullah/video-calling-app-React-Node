533c8c35948c0d9232820f3865215008903041aa
Ahmed067Abdullah <ahmed@sudofy.com>
2020-08-15 00:10:52 +0500

    deployment fix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = require("express")();
+const express = require("express");
+
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
