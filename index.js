aced7390ef852f9b514859d2c5a80fb852ee7a53
Ahmed067Abdullah <ahmed@sudofy.com>
2020-08-15 00:38:26 +0500

    fix

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/home", (req, res) => res.json({ "hello": "world" }));
 
 // for production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("./client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
