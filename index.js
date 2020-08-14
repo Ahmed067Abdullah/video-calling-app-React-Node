ea32578b1966196a517810a48c28dd32bf165240
Ahmed067Abdullah <ahmed@sudofy.com>
2020-08-15 00:37:39 +0500

    path fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/home", (req, res) => res.json({ "hello": "world" }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
